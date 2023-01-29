ee2b4db9833418a33f8c301dd9ea8b13cab38315
Sarath Kumar Mannam <saru@Badminton.local>
2023-01-29 16:05:16 -0500

    217

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 // Problem. Leetcode 217. Contains Duplicate
 // Complexity 
-// 1. Using Set - O(n) Time and O(n) Set
+// 1. Using Set - O(n) Time and O(n) Space
+// 2. Sorting array - O(nlogn) Time and O(1) Space
 class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
