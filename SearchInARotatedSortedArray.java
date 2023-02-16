7e768512244571939ffd15dc0065e98c4dc4ce09
Sarath M <94332524+pheonix-18@users.noreply.github.com>
2023-02-16 07:06:50 -0500

    Create SearchInARotatedSortedArray.java

---
+++ SearchInARotatedSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int mid, left = 0, right = nums.length - 1;
+        while(left < right){
+            mid = left + right;
+            mid /= 2;
+
+            if(nums[mid] < nums[right])
+                right = mid;
+            else
+                left = mid + 1;
+
+        }
+        return nums[left];
+    }
+}
